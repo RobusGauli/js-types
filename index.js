9fd6b67539746a28a5b4db0612426ddc962b3919
Robus Gauli <robusgauli@gmail.com>
2018-06-18 09:28:47 +0545

    undefined contract

--- index.js
+++ index.js
@@ -4,47 +4,49 @@
  * This module provides the helper functions to validate your primitives in JS
  */
 
-
-
-const String = undefinedContract(Type('string'))
-const Number = Type('number');
-const Boolean = Type('boolean');
+const String = undefinedContract(Type("string"));
+const Number = Type("number");
+const Boolean = Type("boolean");
 
 function errorHandler(errorType) {
   return {
     error: true,
     type: errorType
-  }
+  };
 }
 
 function undefinedContract(func) {
   return function(validationRule) {
     const oldFunction = func(validationRule);
-    return function(oldFunction) {
-      return function(data) {
-        if (
-          data === undefined ||
-          data.toString() === 'undefined'
-        ) {
-          return errorHandler('undefined');
-        }
-        return oldFunction(data);
+    function validate(data) {
+      if (data === undefined || data.toString() === "undefined") {
+        return errorHandler("undefined");
       }
+      return oldFunction(data);
+    };
+    return {
+      validate
     }
-  }
+  };
 }
 
 function Type(type) {
   return function(validationRule) {
-    return function(data) {
+    function validate(data) {
       // first check if the data has undefined
       return {
         error: false,
         result: null
-      }
+      };
+    };
+    return {
+      validate,
     }
-  }
+  };
 }
 
+function main() {
+  console.log(String({}).validate('this is the value'));
+}
 
-
+main();
